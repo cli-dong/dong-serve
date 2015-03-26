0cbda8d710b4bc9bcec23216b89268f15de40054
crossjs <liwenfu@crossjs.com>
2015-03-26 19:08:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# dong-serve
+
+[![NPM version](https://img.shields.io/npm/v/dong-serve.svg?style=flat-square)](https://npmjs.org/package/dong-serve)
+
+> a web server with static serve and api mocking
+
+## Usage
+
+see [dong](https://github.com/crossjs/dong)
---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+ * dong-serve
+ * https://github.com/crossjs/dong-serve
+ *
+ * Copyright (c) 2015 crossjs
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var extend = require('extend')
+var getPkg = require('package')
+
+module.exports = function(options) {
+
+  var pkg = getPkg('.')
+
+  options = extend({
+    root: '.',
+    host: '127.0.0.1',
+    port: 9527,
+    mock: 'api',
+    open: false
+  }, pkg && pkg.dong || {}, options)
+
+  require('./lib/server')(options)
+
+}
